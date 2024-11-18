558f4c84024d76a9132ee20d149b57d7693636d1
Heloysa <heloysagama2@gmail.com>
2024-11-18 11:36:29 -0300

    teste

--- server.js
+++ server.js
@@ -13,25 +13,21 @@ function getAparelhoInfo(serial, modelo = "SEM_DADOS") {
             card = "1"
             vlan = 1000
             olt = "OLT-NOKIA-01"
-            pon = "1/1/1"
         } else {
             card = "14"
             vlan = 1000
             olt = "OLT-NOKIA-02"
-            pon = "1/1/2"
         }
     } else if (serial.includes("DACM")) {
         vendor = "DATACOM"
         card = "1"
         vlan = 1000
         olt = "OLT-DATACOM-01"
-        pon = "1/1/3"
     } else if (serial.includes("HWTC")) {
         vendor = "HUAWEI"
         card = "6"
         vlan = 100
         olt = "OLT-HUAWEI-01"
-        pon = "1/1/4"
     }
 
     return { vendor, modelo, card, vlan, olt, pon }
@@ -50,12 +46,10 @@ app.post('/aparelho-info', async (req, res) => {
 
     const aparelhoInfo = getAparelhoInfo(serial, modelo)
     const mac = await getMac(serial)
-    const ip = `192.168.1.${Math.floor(Math.random() * 254) + 1}`
 
     const response = {
         ...aparelhoInfo,
         mac,
-        ip
     }
 
     res.json(response)
